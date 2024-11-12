4073b677a3a546f14d0aa00f16c1028ef19d3ee1
Peter Smith <peter@cruelandunusual.be>
2024-11-12 15:08:38 +0000

    initial commit

---
+++ ch01/values_types_operators.js
@@ -0,0 +1,14 @@
+console.log("-----------");
+
+let x = Math.round(Math.random()*100);
+let y = Math.round(Math.random()+1);
+if (x < (100/y))
+{
+    console.log(x + " is less than " + 100/y);
+}
+else
+{
+    console.log("ha ha, x is way too big");
+}
+
+console.log("-----------");
