7eb8b930af515f14910689378b88e6a93e82c567
Peter Smith <peter@cruelandunusual.be>
2024-11-18 16:43:20 +0000

    more values, types and operators

--- ch01/values_types_operators.js
+++ ch01/values_types_operators.js
@@ -1,6 +1,7 @@
-/*
-console.log("-----------");
 
+/* 
+// Simple logial test and if else structure
+ 
 let x = Math.round(Math.random()*100);
 let y = Math.round(Math.random()+1);
 if (x < (100/y))
@@ -11,10 +12,11 @@ else
 {
     console.log("ha ha, x is way too big");
 }
+ */
 
-console.log("-----------");
-*/
+
 /* 
+// Factorial function, just to remind myself how recursion works
 function factorial(n)
 {
     if (n == 0)
@@ -31,6 +33,11 @@ function factorial(n)
 var num = 5;
 console.log(factorial(num));
  */
+
+
+/* 
+// Building a string up from various substrings based on result of logical test.
+// Also refresher on using ternary logical operator
 let x = Math.floor(Math.random() * 10);
 let y = Math.floor(Math.random() * 10);
 resultStr = x + " > " + y;
@@ -39,10 +46,41 @@ console.log(x>y ? resultStr + " = true" : resultStr + " = false");
 console.log("-----------");
 
 console.log(!(x>y));
+ */
 
-/*if (x > y) {
-    console.log(resultStr + " = true");
+/* 
+// NaN -- somehow this doesn't work for values that aren't a number.
+// e.g. the test !Number.isNaN(num) printed inside a console.log will
+// print the properly expected result, but the test inside an if/else
+// just enters the if block every time...
+let num = 5;
+if (!Number.isNaN(num)){
+    console.log(num + " is the square root of " + num * num);
 }
-else console.log(resultStr + " = false");
-*/
+else {
+    console.log("you should have entered a number");
+}
 
+// more branching stuff generally
+// I know all this stuff but I'm being a good student
+// and covering it again for completeness
+let myNum = 1;
+if (myNum < 10){
+    console.log("small");
+}
+else if (myNum < 50){
+    console.log("medium");
+}
+else {
+    console.log("big");
+}
+ */
+
+
+// looping
+let number = 0;
+while (number < 12){
+    
+    number += 2;
+    console.log(number);
+}
