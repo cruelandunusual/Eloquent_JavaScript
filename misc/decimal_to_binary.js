b62e23335e7d69d29c1aa8dbdb4d060abf3baff4
Peter Smith <peter@cruelandunusual.be>
2024-12-09 18:27:25 +0000

    decimal to binary and back again

--- misc/decimal_to_binary.js
+++ misc/decimal_to_binary.js
@@ -1,4 +1,4 @@
-function binary_to_decimal(num_as_bin) {
+/* function binary_to_decimal(num_as_bin) {
     let left = "";
     let right = ""
     let split = num_as_bin.length / 2;
@@ -9,7 +9,51 @@ function binary_to_decimal(num_as_bin) {
     left = Number(left);
     right = Number(right);
     return (2 * left) + right;
+} */
+
+
+
+function binary_to_decimal(binary) {
+    let result = 0;
+    let multiplier = 1;
+    for (let i = binary.length - 1; i >= 0; i--) {
+        result += binary[i] * multiplier;
+        multiplier *= 2;
+    }
+    return result;
 }
 
-myString = "10";
+myString = "1101010101010010101010101010";
 console.log(binary_to_decimal(myString));
+
+
+
+function decimal_to_binary(decimal) {
+    const bitlength = 16;
+    let bit = '0';
+    const binary = NewArray(bitlength, bit);
+    for (let i = bitlength-1; i >= 0; decimal = Math.floor(decimal / 2), i--) {
+        if (decimal % 2 == 0) {
+            bit = '0';
+        }
+        else {
+            bit = '1';
+        }
+        binary[i] = bit;
+    }
+    let binaryStr = '';
+    for (let i = 0; i < bitlength; i++) {
+        binaryStr += binary[i];
+    }
+    return binaryStr;
+}
+
+console.log(decimal_to_binary(35541));
+
+function NewArray(size, bit) {
+    let x = [];
+    for (let i = 0; i < size; i++) {
+        x[i] = bit;
+    }
+    return x;
+}
