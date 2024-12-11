b513b4120ca6e45da7e7236fb399aaaffb402b7b
Peter Smith <peter@cruelandunusual.be>
2024-12-11 18:09:54 +0000

    more decimal to binary stuff

--- misc/decimal_to_binary.js
+++ misc/decimal_to_binary.js
@@ -1,3 +1,10 @@
+/*
+Aborted attempt at using recursion to split a
+string recursively into chunks of two cells,
+compute the results of each two-cell unit,
+and then compute the result of the whole thing.
+I might return to this, it's an interesting challenge.
+*/
 /* function binary_to_decimal(num_as_bin) {
     let left = "";
     let right = ""
@@ -11,8 +18,14 @@
     return (2 * left) + right;
 } */
 
+/////////////////////////////////////////////////////////////
 
-
+/*
+Classic binary to decimal algorithm,
+multiplying the value of each position
+by an increasing power of two.
+*/
+/* 
 function binary_to_decimal(binary) {
     let result = 0;
     let multiplier = 1;
@@ -23,11 +36,17 @@ function binary_to_decimal(binary) {
     return result;
 }
 
-myString = "1101010101010010101010101010";
-//console.log(binary_to_decimal(myString));
+// myString = "1101010101010010101010101010";
+// console.log(binary_to_decimal(myString));
+*/
 
+/////////////////////////////////////////////////////////////
 
-
+/*
+I think this algorithm comes from K&R.
+The NewArray function is something I found on stackexchange
+*/
+/*
 function decimal_to_binary(decimal) {
     const bitlength = 16;
     let bit = '0';
@@ -48,18 +67,22 @@ function decimal_to_binary(decimal) {
     return binaryStr;
 }
 
-// console.log(decimal_to_binary(process.argv[2]));
-
-
-function NewArray(size, bit) {
+function NewArray(size, data) {
     let x = [];
     for (let i = 0; i < size; i++) {
-        x[i] = bit;
+        x[i] = data;
     }
     return x;
 }
 
+// console.log(decimal_to_binary(process.argv[2]));
+*/
 
+/////////////////////////////////////////////////////////////
+
+/*
+My idea 
+*/
 let lookUpTable = {
     b1: "0001",
     b2: "0010",
@@ -76,62 +99,82 @@ let lookUpTable = {
 // console.log(lookUpTable.b1);
 // console.log(0 || 0);
 
+/*
+Testing the binaryAddTwoBits function separately
+(it's part of a bigger solutions and embedded inside
+another function below)
+*/
+
 /* function binaryAddTwoBits(op1, op2, carry) {
+    op1 = Number(op1);
+    op2 = Number(op2);
+    carry = Number(carry);
     let result = 0;
-    answer = [];
-    if (op1 && op2) {
-        result = 0, carry = 1;
+    let tempResultArray = [result, carry];
+    if (op1 && op2 && carry) {
+        result = '1', carry = '1';
+    }
+    else if (op1 && op2) {
+        result = '0', carry = '1';
+    }
+    else if ((op1 || op2) && carry) {
+        result = '1', carry = '0';
     }
     else if (op1 || op2) {
-        result = 1, carry = 0;
+        result = '1', carry = '0';
     }
     else {
-        result = carry = 0;
+        result = carry = '0';
     }
-    answer = [carry, result];
-    return answer;
+    tempResultArray = [result, carry];
+    return tempResultArray;
 }
+
+console.log(binaryAddTwoBits(1, 1, 0));
+console.log('0' && '0' && '0' && '0');
  */
 
 
-// console.log(binaryAddTwoBits(1, 1));
-//console.log('0' && '0' && '0' && '0');
+const INDEX_RESULT = 0;
+const INDEX_CARRY = 1;
 
 function addBinaryNumbers(op1, op2, carry = 0) {
     let result = 0;
-    let tempResultArray = [carry, result];
+    let finalArray = [];
+    let tempResultArray = [result, carry];
     let resultStr = "";
     const bitlength = 4;
-    function binaryAddTwoBits(op1, op2, carry = 0) {
+    function binaryAddTwoBits(op1, op2, carry) {
         op1 = Number(op1);
         op2 = Number(op2);
         carry = Number(carry);
-        if (op1 && op2 && carry) {
-            result = '1', carry = '1';
-        }
-        else if (op1 && op2) {
-            result = '0', carry = '1';
-        }
-        else if (op1 || op2) {
-            result = '1', carry = '0';
-        }
-        else {
-            result = carry = '0';
-        }
-        tempResultArray = [carry, result];
+    if (op1 && op2 && carry) {
+        result = '1', carry = '1';
+    }
+    else if (op1 && op2) {
+        result = '0', carry = '1';
+    }
+    else if ((op1 || op2) && carry) {
+        result = '0', carry = '1';
+    }
+    else if (op1 || op2) {
+        result = '1', carry = '0';
+    }
+    else {
+        result = carry = '0';
+    }
+        tempResultArray = [result, carry];
         return tempResultArray;
     }
     for (let i = bitlength-1; i >= 0; i--) {
-        result = binaryAddTwoBits(op1[i], op2[i], tempResultArray[carry]);
-        resultStr += result[1];
+        finalArray = binaryAddTwoBits(op1[i], op2[i], tempResultArray[INDEX_CARRY]);
+        resultStr += finalArray[INDEX_RESULT];
     }
-    if (result[0] == '1') {
-        resultStr += result[0]
+    if (finalArray[1] == '1') {
+        resultStr += finalArray[INDEX_CARRY]
     }
     return resultStr.split("").reverse().join("");
-
 }
-
 
 //console.log(binaryAddTwoBits(1, 0, 0));
 console.log(addBinaryNumbers(lookUpTable.b1, lookUpTable.b3));
